992a4748eded9b3cc7ad37d9891852a16801ee1d
GraceeFlower <1491644103@qq.com>
2020-02-11 09:58:21 +0800

    [WM] refactor: make the params in class private

--- interface/src/main/java/Person.java
+++ interface/src/main/java/Person.java
@@ -1,10 +1,26 @@
 public class Person {
 
-    String name;
-    int money;
+    private String name;
+    private int money;
 
     public Person(String name, int money) {
         this.name = name;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
         this.money = money;
     }
 
